4539aa98aa484383c3de1647171f50117f96f580
DaSteff91 <stefan.merthan@yahoo.de>
2026-01-17 22:19:15 -0300

    chore: manual sync of version to v1.4.0 and fix release config

--- src/__init__.py
+++ src/__init__.py
@@ -12,7 +12,7 @@ from flask import Flask
 from src.config import Config
 from src.extensions import db
 
-__version__ = "1.2.0"
+__version__ = "1.4.0"
 
 
 def create_app(config_class: type[Config] = Config) -> Flask:
